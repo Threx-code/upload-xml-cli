a82ef51afd36afa71d1d8d0e614941fd5c0bd171
Oluwatosin Emmanuel Amokeodo <oluwatosin.amokeodo@gmail.com>
2023-04-26 14:01:22 +0100

    Update XMLInterface.php

--- app/Contracts/XMLInterface.php
+++ app/Contracts/XMLInterface.php
@@ -4,7 +4,4 @@ namespace App\Contracts;
 
 interface XMLInterface
 {
-    public static function getGoogleURL();
-    public static function authenticateUser($request);
-    public static function uploadXMLFileToGoogleSheet($data);
 }
